1d55567dc0543b488be14be80496166c39f4bfd2
agordavidt <agordavid14@gmail.com>
2023-06-26 15:01:05 +0000

    update task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrfalse) {
 	var task2 = true;
 
 	if (trueOrFalse) {
-		let task = true;
-		let task2 = false;
+		var innerTask = true;
+		let innerTask2 = false;
+		task = innerTask;
+		task2 = innerTask2;
 	}
 
 	return [task, task2];
