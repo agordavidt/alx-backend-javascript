6f6b2817c477e88437d579af2e5a8d702e982f0e
agordavidt <agordavid14@gmail.com>
2023-07-04 01:28:40 +0000

    task 0 updated

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,7 @@
-module.exports = {
-	  presets: [
-		      [
-			            '@babel/preset-env',
-			            {
-					            targets: {
-							              node: 'current',
-							            },
-					          },
-			          ],
-		    ],
-};
+const getListStudents = () => [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
+
+export default getListStudents;
